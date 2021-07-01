7f6f26f229a0c5100285c0e8944747fdff448d7e
Ivan Osokin <osokin.ivan.nstu@gmail.com>
2021-07-01 14:43:07 +0700

    Убрал ненужные тесты.

--- Lesson3_Task1/Tests/Counting_Digits_Test.cpp
+++ Lesson3_Task1/Tests/Counting_Digits_Test.cpp
@@ -1,34 +1,9 @@
 #include "gtest/gtest.h"
 #include "Count_Digits.h"
-/*
-TEST(GetIntegerTestCase, Test_1)
-{
-    int n = getNumber();
-    EXPECT_EQ(n, -1);
-}
 
-TEST(GetIntegerTestCase, Test_2)
-{
-    int n = getNumber();
-    EXPECT_EQ(n, 0);
-}
-
-TEST(GetIntegerTestCase, Test_3)
-{
-    int n = getNumber();
-    EXPECT_EQ(n, 1);
-}
-
-TEST(GetIntegerTestCase, Test_4)
-{
-    int n = getNumber();
-    EXPECT_EQ(n, 12345);
-}
-*/
 
 TEST(GetCountTestCase, Test_1)
 {
-    //int n = countDigits(1);
     EXPECT_EQ(countDigits(1), 1);
 }
 
@@ -39,5 +14,5 @@ TEST(GetCountTestCase, Test_2)
 
 TEST(GetCountTestCase, Test_3)
 {
-    EXPECT_EQ(countDigits(12345), 4);
+    EXPECT_EQ(countDigits(12345), 5);
 }
