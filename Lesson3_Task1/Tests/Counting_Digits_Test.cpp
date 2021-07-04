9b5592fcf77e858a7acac9b2a840df9cdab1f367
Ivan Osokin <osokin.ivan.nstu@gmail.com>
2021-07-04 23:09:54 +0700

    Добавил тесты на 0 и на отрицательные числа.

--- Lesson3_Task1/Tests/Counting_Digits_Test.cpp
+++ Lesson3_Task1/Tests/Counting_Digits_Test.cpp
@@ -16,3 +16,13 @@ TEST(GetCountTestCase, Test_3)
 {
     EXPECT_EQ(countDigits(12345), 5);
 }
+
+TEST(GetCountTestCase, Test_4)
+{
+    EXPECT_EQ(countDigits(0), 1);
+}
+
+TEST(GetCountTestCase, Test_5)
+{
+    EXPECT_EQ(countDigits(-345), 3);
+}
--- Lesson3_Task3/main.cpp
+++ Lesson3_Task3/main.cpp
@@ -8,33 +8,12 @@
 
 #include <stdio.h>
 #include <math.h>
+#include "Exchange_Digits.h"
 
 int main()
 {
-    int number;
-    do
-    {
-        printf("Enter a number (must be greater than 0): ");
-        if(scanf("%d", &number) == 0 || number < 1)
-        {
-            printf("The enter is incorrect!!!\nTry to enter again!!!\n\n");
-            fflush(stdin);
-        }
-    }while(number < 1);
-
-    int i = 0;
-    int tempNum = number;
-    while(tempNum /= 10)
-    {
-        ++i;
-    }
-
-    const int firstNum = number / pow(10, i);
-    const int middleNum = (number / 10 - firstNum * pow(10, i - 1)) * 10;
-    const int lastNum = number % 10;
-
-    number = lastNum * pow(10, i) + middleNum + firstNum;
-    printf("\nA new number is: %d", number);
+    int number = getInteger();
+    printf("\nA new number is: %d", exchangeDigits(number));
 
     return 0;
 }
